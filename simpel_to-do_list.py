02563d3201094cf39a02560e08c20fd1a001bb42
Wildan6666 <wildancuy26@gmail.com>
2024-07-16 00:33:56 +0700

    tambah function

--- simpel_to-do_list.py
+++ simpel_to-do_list.py
@@ -1,9 +1,12 @@
 #Projek to-do-list
+import sys
+
 print("Selamat datang di To-Do List")
-print("Apakah ada yang ingin anda lakukan hari ini : Y")
+print("Apakah ada yang ingin anda lakukan hari ini : ")
 ulang=input()
 simpan=[]
-while ulang=='Y':
+
+def menu():
     print('\nPilih salah satu dari berikut: ')
     print('1. Input To-Do list')
     print('2. Hapus To-Do list')
@@ -11,52 +14,71 @@ while ulang=='Y':
     print('4. Ubah To-Do list')
     print('5. Akhiri program')
 
-    pilih=input("Masukkan pilihan:")
-
-    if pilih=='1':
-        print('\nMasukkan To-Do list: ')
-        todo=input()
-        simpan.append(todo)
-        
-    elif pilih=='2':
-        print('Yang ingin anda hapus:\n1.Hapus awal \n2.Hapus akhir \n3.Hapus semuanya ')
-        hapus=input()
-        if hapus=='1':
-            del simpan[0]
-        elif hapus=='2':
-            del simpan[-1]
-        elif hapus=='3':
-            simpan.clear()
-        else:
-            print('PILIH 1 - 3')
+   
+def Masuk():
+    todo=input()
+    simpan.append(todo)
     
-    elif pilih=='3':
-        if simpan:
-            for i in enumerate(simpan,start=1):
+def hapus():
+    for i in enumerate(simpan,start=1):
+         print(i)
+    
+    try:
+        hapus=int(input('Pilih no yang ingin di hapus : '))
+        index=int(hapus-1)
+        if 0 <= index < len(simpan):
+            del simpan[index]
+        else:
+            print('Nomor tidak valid')
+    except ValueError:
+        print("Masukkan angka yang valid.")
+            
+def lihat():
+    if simpan:
+        for i in enumerate(simpan,start=1):
                 print(i)
         else:
             print('Tidak ada To-Do list hari ini')
-            
-    elif pilih=='4':
-        for i in enumerate(simpan,start=1):
+
+def ubah():
+     for i in enumerate(simpan,start=1):
                 print(i)
-        print('Pilih To-Do yang ingin diubah:')
-        ubah=input()
-        index = int(ubah)-1
-        if index<len(simpan):
+     print('Pilih To-Do yang ingin diubah:')
+     ubah=input()
+     index = int(ubah)-1
+     if index<len(simpan):
             print('Masukan To-Do yang baru: ')
             baru=input('')
             simpan[index]=baru
-            
+
+def akhiri():
+    print("Program selesai. Terima kasih!")
+    sys.exit()
+
+
+def main():
+    menu()
+    pilih=input("Masukkan pilihan:")
+    if pilih=='1':
+        Masuk()
+    elif pilih=='2':
+        hapus()
+    elif pilih=='3':
+        lihat()
+    elif pilih=='4':
+        ubah()
     elif pilih=='5':
-        break
+        akhiri()
     else:
-        print('PILIH 1 - 5')
-
-
+        print('Pilih 1 - 5')
+  
         
-            
+while ulang:
+    main()
+
+
     
+
     
     
     
